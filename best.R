0879709477333771097b4b2852a48f4cd8001c11
dahhn <daniel.hahn86@gmail.com>
2022-01-23 18:41:31 -0600

    finished function and tested

--- best.R
+++ best.R
@@ -6,31 +6,29 @@ best <- function(stateCode, disease) {
   colnames(outcomeDF) <-c("HOSPITAL","STATE","HEART ATTACK","HEART FAILURE","PNEUMONIA")
   outcomeDF[outcomeDF == "Not Available"] <- NA
   outcomeDF[,3:5] <- outcomeDF[,3:5] %>% mutate_if(is.character,as.numeric)
-  vali(stateCode, disease)
   ## Check that state and outcome are valid
   vali <- function(st, oc){
-    library("dplyr")
-    `%notin%` <- Negate('%in%')
-    st <- toupper(st)
-    oc <- toupper(oc)
-    if(st %notin% outcomeDF$STATE){
-      stop("invalid outcome")
-    } else if(oc %notin% colnames(outcomeDF)) {
-      stop("invalid outcome")
-    } else 
-      calc(st,oc)
-  }
+            library("dplyr")
+            `%notin%` <- Negate('%in%')
+            st <- toupper(st)
+            oc <- toupper(oc)
+            if(st %notin% outcomeDF$STATE){
+                      stop("invalid state")
+            } else if(oc %notin% colnames(outcomeDF)) {
+                      stop("invalid outcome")
+            } else 
+                      calc(st,oc)
+    }
+  ## Return hospital name in that state with lowest 30-day death rate
   calc <- function(st,oc){
-    filter(outcomeDF, outcomeDF$STATE == st) %>% 
-    na.omit(outcomeDF)  
+            filtered <- filter(outcomeDF, outcomeDF$STATE == st) %>% 
+            na.omit(filtered) %>%
+            select(., HOSPITAL, {{oc}})
+            minIndex <- which.min(filtered[,2])
+            filtered[minIndex,1]
   }
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  vali(stateCode, disease)
 }
-
-
-
-
 
 
 
