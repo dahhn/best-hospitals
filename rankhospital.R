201b119e4543ef7e41bd44a3f02d34f6e1410709
dahhn <daniel.hahn86@gmail.com>
2022-01-25 19:45:56 -0600

    Added code to read and clean data. Added error handling. Filtered data based on arguements.
    Data return and sorting is still incomplete.

--- rankhospital.R
+++ rankhospital.R
@@ -1,6 +1,26 @@
-rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
+rankhospital <- function(st, outcome, num = "best") {
+  require("tidyverse")
+  `%notin%` <- Negate(`%in%`)
+  ## Read outcome data to OC, name columns, change char to numeric
+  oc <- read.csv("outcome-of-care-measures.csv") %>%
+        select(., Hospital.Name, State, starts_with("Hospital.30.Day.Death"))
+  colnames(oc) <- c("HOSPITAL","STATE","HEART ATTACK","HEART FAILURE","PNEUMONIA") 
+  oc[oc == "Not Available"] <- NA
+  oc[,3:5] <- oc[,3:5] %>% mutate_if(is.character,as.numeric)
   ## Check that state and outcome are valid
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
+  st <-toupper(st) 
+  outcome <- toupper(outcome)
+  if(st %notin% oc$STATE) {stop("invalid state")}
+  if(outcome %notin% colnames(oc)){stop("invalid outcome")}
+  # Return hospital name in that state with the given rank 30-day death rate
+  filtered <- filter(oc, oc$STATE == st) %>%
+    na.omit(filtered) %>%
+    select(., HOSPITAL, {{outcome}})
+  #order(filtered,filtered[1], filtered$HOSPITAL)
+  returnDF <- filtered[order(filtered[2]),filtered[1]] %>% 
+  
+head(returnDF)
 }
+
+
+rankhospital("UT", "Heart Attack", 4)
