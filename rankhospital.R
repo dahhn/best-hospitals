b0ae65ddefe096478ce8074b06263f576733ed64
dahhn <daniel.hahn86@gmail.com>
2022-02-01 14:07:34 -0600

    Moved state and outcome variable validation below oc definition.

--- rankhospital.R
+++ rankhospital.R
@@ -4,16 +4,16 @@ rankhospital <- function(st, outcome, n = "BEST") {
   st <-toupper(st)
   outcome <- toupper(outcome)
   
-  ## Check that state and outcome are valid 
-  if(st %notin% oc$STATE) {stop("invalid state")}
-  if(outcome %notin% colnames(oc)) {stop("invalid outcome")}
-
   ## Read outcome data to OC, name columns, change char to numeric
   oc <- read.csv("outcome-of-care-measures.csv") %>%
           select(., Hospital.Name, State, starts_with("Hospital.30.Day.Death"))
   colnames(oc) <- c("HOSPITAL","STATE","HEART ATTACK","HEART FAILURE","PNEUMONIA") 
   oc[oc == "Not Available"] <- NA
   oc[,3:5] <- oc[,3:5] %>% mutate_if(is.character,as.numeric)
+  
+  ## Check that state and outcome are valid 
+  if(st %notin% oc$STATE) {stop("invalid state")}
+  if(outcome %notin% colnames(oc)) {stop("invalid outcome")}
   
   ## Create dataframe to analyze based on state
   filtered <- filter(oc, oc$STATE == st) %>%
