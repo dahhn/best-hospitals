54d50b5590fe72b46409721186cbe735f20f860c
dahhn <daniel.hahn86@gmail.com>
2022-02-07 05:05:28 -0600

    Complete overhaul of function. Both 'best' and 'worst' aspects of function return values as intended. Subseting aspect does not function correctly. Definately can use optimization as function as written is slow.

--- rankall.R
+++ rankall.R
@@ -3,24 +3,71 @@ rankall <- function(outcome, num = "best") {
   library(data.table)
   `%notin%` <- Negate(`%in%`)
   outcome <- toupper(outcome)
-  ## Read outcome data to OC, name columns, change char to numeric
-  oc <- read.csv("outcome-of-care-measures.csv") %>%
-    select(., Hospital.Name, State, starts_with("Hospital.30.Day.Death"))
-  colnames(oc) <- c("HOSPITAL","STATE","HEART ATTACK","HEART FAILURE","PNEUMONIA") 
-  oc[oc == "Not Available"] <- NA
-  oc[,3:5] <- oc[,3:5] %>% mutate_if(is.character,as.numeric)
   
-  ## validate outcome
+  # Read outcome data to OC, name columns, change char to numeric
+  oc <<- read.csv("outcome-of-care-measures.csv") %>%
+    select(., Hospital.Name, State, starts_with("Hospital.30.Day.Death"))
+  colnames(oc) <- c("HOSPITAL","STATE","HEART ATTACK","HEART FAILURE","PNEUMONIA")
+  oc[oc == "Not Available"] <<- NA
+  oc[,3:5] <- oc[,3:5] %>% mutate_if(is.character,as.numeric)
+  oc <<-tibble(oc) %>% group_by(`STATE`)
+  # validate outcome
   if(outcome %notin% colnames(oc)) {stop("invalid outcome")}
   
-  ## Create dataframe to analyze based on state
-  filtered <- na.omit(oc) %>% 
-    select(., HOSPITAL, STATE, `HEART ATTACK`, `HEART FAILURE`, PNEUMONIA) %>% 
-    group_by(`STATE`)
-  if(outcome == "HEART ATTACK"){arrange(filtered, `HEART ATTACK`, .by_group = T)}
-  else if(outcome == "HEART FAILURE"){arrange(filtered, `HEART FAILURE`,.by_group = T)}
-  else if(outcome == "PNEUMONIA"){arrange(filtered, PNEUMONIA,.by_group = T)}
+  # function to evaluate arguements 
+    x<-data.frame()
+    stateList <- unique(oc$STATE)
+    if(outcome == "HEART ATTACK"){
+      selMin <- function(df)return(df[which.min(df$`HEART ATTACK`),])
+      selMax <- function(df)return(df[which.max(df$`HEART ATTACK`),] )
+    }
+    else if(outcome == "HEART FAILURE"){
+      selMin <- function(df)return(df[which.min(df$`HEART FAILURE`),])
+      selMax <- function(df)return(df[which.max(df$`HEART FAILURE`),] )
+    }
+    else if(outcome == "PNEUMONIA"){
+      selMin <- function(df)return(df[which.min(df$`PNEUMONIA`),])
+      selMax <- function(df)return(df[which.max(df$`PNEUMONIA`),]) 
+    }
+
+    if(is.character(num) == T){
+      num <- toupper(num)
+      stateList <- unique(oc$STATE)
+      if(num == "BEST"){
+        for (i in 1:54){
+          df <- filter(oc, STATE == stateList[i])
+          row1 <- selMin(df)
+          x<-rbind(row1,x)
+        }
+        x
+        }
+      else if(num == "WORST"){
+        for (i in 1:54){
+          df <- filter(oc, STATE == stateList[i])
+          row1 <- selMax(df)
+          x<-rbind(row1,x)
+        }
+        
+        }
+      }
+    else {
+      for (i in 1:54){
+        df <- filter(oc, STATE == stateList[i])
+        row1 <- slice(df,n = num)
+        x<-rbind(row1,x)
+      }
+    } 
+    select(x, HOSPITAL, STATE) %>% arrange(STATE)    
 }
+
+rankall("pneumonia", "best")
+tail(rankall("pneumonia", "worst"), 3)
 rankall("heart attack",1)
-rankall("heart failure","worst")
-rankall("pneumonia", "worst")
+rankall("heart attack","best")
+rankall("heart attack","worst")
+rankall("heart failure","best")
+
+
+
+
+
